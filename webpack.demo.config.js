a13ee675c1f9d793cb53647ca0512e3d8a6971a8
Tamara Cook <tamara.cook@icloud.com>
2015-11-10 00:56:47 +0100

    Demo: only one entries, not two

--- webpack.demo.config.js
+++ webpack.demo.config.js
@@ -3,7 +3,7 @@ const html = require('html-webpack-plugin');
 const jade = require('jade');
 const config = require('./webpack.config.js');
 config.entry = {
-  library: './src',
+  //library: './src',
   app: './demo'
 };
 config.output = {
@@ -16,7 +16,11 @@ config.plugins.push(
   new html({
     templateContent: jade.compileFile(__dirname+'/demo/index.jade'),
     title: 'wbt-text Demo App',
-    inject: 'head'
+    inject: 'head',
+    versions: {
+      angular: '1.4.7',
+      bootstrap: '3.3.5'
+    }
   })
 );
 module.exports = config;
