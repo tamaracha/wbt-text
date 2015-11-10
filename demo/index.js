7e245318de029c32f445ffd3738dd5e4b303ecf8
Tamara Cook <tamara.cook@icloud.com>
2015-11-10 01:01:48 +0100

    Extend demo

--- demo/index.js
+++ demo/index.js
@@ -1,15 +1,23 @@
 import angular from 'angular';
-export default angular.module('demoApp',[window.demo.library])
+import templateUrl from './demo.jade';
+import uiBootstrap from 'angular-ui-bootstrap';
+import wbtText from '../src';
+
+export default angular.module('demoApp',[wbtText, uiBootstrap])
 .config(function(markdownProvider){
   markdownProvider.preset = 'commonmark';
 })
-.controller('DemoController',function(){
-  this.text = '# hello markdown';
+.controller('DemoController',function($interval){
+  this.text = '# hello markdown {{demo.text2}}';
+  this.text2 = 0;
+  $interval(() => {
+    this.text2++;
+  },1000,10);
 })
 .directive('demo',function(){
   return {
     controller: 'DemoController as demo',
-    template: require('./demo.jade')
+    templateUrl
   };
 })
 .name;
